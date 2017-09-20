13f341d15448d0c65c6d66b762a9c224e4523476
Andrey_Chadov <Andrey_Chadov@epam.com>
2017-09-20 13:50:36 +0600

    [IMP] First implementation of sprint context with simplest methods

--- src/main/java/com/epam/java/spring/MyApp.java
+++ src/main/java/com/epam/java/spring/MyApp.java
@@ -1,14 +1,16 @@
 package com.epam.java.spring;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * @author Andrey_Chadov on 9/20/2017.
  */
 public class MyApp {
 
     public static void main(String[] args) {
-        //create the object
-        Coach theCoach = new TrackCoach();
-        //use the object
-        System.out.println(theCoach.getDailyWorkout());
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-config.xml");
+        BaseBallCoach baseBallCoach = (BaseBallCoach) applicationContext.getBean("myCoach");
+        System.out.println(baseBallCoach.getDailyWorkout());
     }
 }
