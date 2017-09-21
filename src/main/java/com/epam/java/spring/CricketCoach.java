263a2b852cae650614f953b8871b36265da5b8c7
Andrey_Chadov <Andrey_Chadov@epam.com>
2017-09-21 17:53:31 +0600

    [REF] Refactored

--- src/main/java/com/epam/java/spring/CricketCoach.java
+++ src/main/java/com/epam/java/spring/CricketCoach.java
@@ -13,6 +13,7 @@ public class CricketCoach implements Coach {
     }
 
     public void setFortuneService(FortuneService fortuneService) {
+        System.out.println("CricketCoach: inside setter method - setFortuneService");
         this.fortuneService = fortuneService;
     }
 
