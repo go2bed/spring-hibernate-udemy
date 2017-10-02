403e529e901e9c222cdd23fffb5ad4a9466ad8d8
Andrey_Chadov <Andrey_Chadov@epam.com>
2017-10-02 16:29:11 +0600

    [IMP] Implemented multiple services with qualifier above the field

---
+++ src/main/java/com/epam/java/spring/DatabaseFortuneService.java
@@ -0,0 +1,15 @@
+package com.epam.java.spring;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Andrey_Chadov on 10/2/2017.
+ */
+@Component
+public class DatabaseFortuneService implements FortuneService {
+
+    @Override
+    public String getFortune() {
+        return null;
+    }
+}
--- src/main/java/com/epam/java/spring/TennisCoach.java
+++ src/main/java/com/epam/java/spring/TennisCoach.java
@@ -1,6 +1,7 @@
 package com.epam.java.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 /**
@@ -10,17 +11,12 @@ import org.springframework.stereotype.Component;
 public class TennisCoach implements Coach {
 
     @Autowired
+    @Qualifier("happyFortuneService")
     private FortuneService fortuneService;
 
     public TennisCoach() {
         System.out.println(">> TennisCoach: inside default constructor");
     }
-//
-//    @Autowired
-//    public void doSomeCrazyStuff(FortuneService fortuneService) {
-//        System.out.println(">> TennisCoach: inside doSomeCrazyStuff() method");
-//        this.fortuneService = fortuneService;
-//    }
 
     @Override
     public String getDailyWorkout() {
