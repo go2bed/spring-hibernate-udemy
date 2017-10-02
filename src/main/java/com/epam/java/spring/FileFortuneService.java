b7f191464b97c1d2e94fcec2dbffd37ebe987309
Andrey_Chadov <Andrey_Chadov@epam.com>
2017-10-02 16:59:30 +0600

    [IMP] Implemented file fortunes service

---
+++ src/main/java/com/epam/java/spring/FileFortuneService.java
@@ -0,0 +1,39 @@
+package com.epam.java.spring;
+
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * @author Andrey_Chadov on 10/2/2017.
+ */
+
+@Component
+public class FileFortuneService implements FortuneService {
+
+    private List<String> data = getDataFromFile();
+
+    //create a random number generator
+    private Random random = new Random();
+
+
+    @Override
+    public String getFortune() {
+        return data.get(random.nextInt(data.size()));
+    }
+
+    private List<String> getDataFromFile() {
+        List<String> lines = null;
+        try {
+            lines = Files.readAllLines(
+                    Paths.get("D:\\LEARN_PROJECTS\\SPRING\\spring-demo-one\\src\\main\\resources\\fortunes.txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lines;
+    }
+}
--- src/main/java/com/epam/java/spring/TennisCoach.java
+++ src/main/java/com/epam/java/spring/TennisCoach.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 public class TennisCoach implements Coach {
 
     @Autowired
-    @Qualifier("randomService")
+    @Qualifier("fileFortuneService")
     private FortuneService fortuneService;
 
     public TennisCoach() {
