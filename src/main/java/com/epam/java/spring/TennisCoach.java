0304f75f5981fc53d7157161cb1970311308b0ae
Andrey_Chadov <Andrey_Chadov@epam.com>
2017-10-02 16:13:06 +0600

    [REF] Some refactoring

--- src/main/java/com/epam/java/spring/TennisCoach.java
+++ src/main/java/com/epam/java/spring/TennisCoach.java
@@ -18,6 +18,7 @@ public class TennisCoach implements Coach {
     //define a setter method
     @Autowired
     public void setFortuneService(FortuneService fortuneService) {
+        System.out.println(">> TennisCoach: inside setFortuneService() method");
         this.fortuneService = fortuneService;
     }
 
