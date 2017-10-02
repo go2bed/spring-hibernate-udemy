03885e5b954df695b693b5fa3e093d39ade3947a
Andrey_Chadov <Andrey_Chadov@epam.com>
2017-10-02 17:13:49 +0600

    [IMP] Implemented post construct and pre destroy methods

--- src/main/java/com/epam/java/spring/TennisCoach.java
+++ src/main/java/com/epam/java/spring/TennisCoach.java
@@ -2,19 +2,33 @@ package com.epam.java.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 /**
  * @author Andrey_Chadov on 10/2/2017.
  */
 @Component
-@Scope("prototype")
 public class TennisCoach implements Coach {
 
     @Autowired
     @Qualifier("fileFortuneService")
     private FortuneService fortuneService;
+
+
+    //define my init method
+    @PostConstruct
+    public void doMyStartUpStuff(){
+        System.out.println(">>> TennisCoach: inside of doMyStartUpStuff()");
+    }
+
+    //define my destroy method
+    @PreDestroy
+    public void doMyCleanupStuff(){
+        System.out.println(">>> TennisCoach: inside of doMyCleanupStuff()");
+    }
 
     public TennisCoach() {
         System.out.println(">> TennisCoach: inside default constructor");
